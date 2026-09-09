1e13de0b4baf34bee13fc162e7c40dd2a1fac90c
Pxatric-BN <chayanon.bn@gmail.com>
2026-09-09 09:04:55 +0700

    feat:profile page

---
+++ src/components/profile/ProfileCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card class="profile-card">
+    <ProfileHeader />
+    <ProfileInfo />
+  </v-card>
+</template>
+
+<script>
+import ProfileHeader from './ProfileHeader.vue'
+import ProfileInfo from './ProfileInfo.vue'
+
+export default {
+  name: 'ProfileCard',
+
+  components: {
+    ProfileHeader,
+    ProfileInfo
+  }
+}
+</script>
+
+<style scoped>
+.profile-card {
+  width: 350px;
+  border-radius: 18px;
+  overflow: visible;
+  background: #ffffff;
+  box-shadow: 0 10px 30px rgba(0, 0, 0, 0.12) !important;
+}
+</style>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="profile-page">
+
+    <ProfileCard />
+
+  </div>
+</template>
+
+<script>
+import ProfileCard from '@/components/profile/ProfileCard.vue'
+
+export default {
+  name: 'Profile',
+
+  components: {
+    ProfileCard
+  }
+}
+</script>
+
+<style scoped>
+.profile-page {
+  min-height: 100vh;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background: #eeeeee;
+}
+</style>
